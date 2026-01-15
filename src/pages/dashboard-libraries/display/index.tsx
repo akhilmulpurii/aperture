bc654ad97a4ad878fc4c2308c0861a8712aedcf3
S.A.M <akhilfilms02@gmail.com>
2026-01-15 15:27:05 -0600

    fix width

--- src/pages/dashboard-libraries/display/index.tsx
+++ src/pages/dashboard-libraries/display/index.tsx
@@ -37,7 +37,7 @@ export default function LibrariesDisplayPage() {
   }
 
   return (
-    <div className="w-full max-w-2xl pb-10">
+    <div className="w-full pb-10">
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <div className="rounded-2xl border border-border/70 bg-background/70 p-5 shadow-sm space-y-6">
@@ -49,7 +49,7 @@ export default function LibrariesDisplayPage() {
               </p>
             </div>
 
-            <div className="space-y-4">
+            <div className="space-y-6">
               <FormField
                 control={form.control}
                 name="metadata.UseFileCreationTimeForDateAdded"
@@ -83,118 +83,120 @@ export default function LibrariesDisplayPage() {
                 )}
               />
 
-              <FormField
-                control={form.control}
-                name="configuration.EnableFolderView"
-                render={({ field }) => (
-                  <FormItem className="flex flex-row items-start space-x-3 space-y-0">
-                    <FormControl>
-                      <Checkbox
-                        checked={field.value}
-                        onCheckedChange={field.onChange}
-                      />
-                    </FormControl>
-                    <div className="space-y-1 leading-none">
-                      <FormLabel>
-                        Display a folder view to show plain media folders
-                      </FormLabel>
-                      <FormDescription>
-                        Display folders alongside your other media libraries.
-                        This can be useful if you'd like to have a plain folder
-                        view.
-                      </FormDescription>
-                    </div>
-                  </FormItem>
-                )}
-              />
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <FormField
+                  control={form.control}
+                  name="configuration.EnableFolderView"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-row items-start space-x-3 space-y-0">
+                      <FormControl>
+                        <Checkbox
+                          checked={field.value}
+                          onCheckedChange={field.onChange}
+                        />
+                      </FormControl>
+                      <div className="space-y-1 leading-none">
+                        <FormLabel>
+                          Display a folder view to show plain media folders
+                        </FormLabel>
+                        <FormDescription>
+                          Display folders alongside your other media libraries.
+                          This can be useful if you'd like to have a plain folder
+                          view.
+                        </FormDescription>
+                      </div>
+                    </FormItem>
+                  )}
+                />
 
-              <FormField
-                control={form.control}
-                name="configuration.DisplaySpecialsWithinSeasons"
-                render={({ field }) => (
-                  <FormItem className="flex flex-row items-start space-x-3 space-y-0">
-                    <FormControl>
-                      <Checkbox
-                        checked={field.value}
-                        onCheckedChange={field.onChange}
-                      />
-                    </FormControl>
-                    <div className="space-y-1 leading-none">
-                      <FormLabel>
-                        Display specials within seasons they aired in
-                      </FormLabel>
-                    </div>
-                  </FormItem>
-                )}
-              />
+                <FormField
+                  control={form.control}
+                  name="configuration.DisplaySpecialsWithinSeasons"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-row items-start space-x-3 space-y-0">
+                      <FormControl>
+                        <Checkbox
+                          checked={field.value}
+                          onCheckedChange={field.onChange}
+                        />
+                      </FormControl>
+                      <div className="space-y-1 leading-none">
+                        <FormLabel>
+                          Display specials within seasons they aired in
+                        </FormLabel>
+                      </div>
+                    </FormItem>
+                  )}
+                />
 
-              <FormField
-                control={form.control}
-                name="configuration.EnableGroupingMoviesIntoCollections"
-                render={({ field }) => (
-                  <FormItem className="flex flex-row items-start space-x-3 space-y-0">
-                    <FormControl>
-                      <Checkbox
-                        checked={field.value}
-                        onCheckedChange={field.onChange}
-                      />
-                    </FormControl>
-                    <div className="space-y-1 leading-none">
-                      <FormLabel>Group movies into collections</FormLabel>
-                      <FormDescription>
-                        Movies in a collection will be displayed as one grouped
-                        item when displaying movie lists.
-                      </FormDescription>
-                    </div>
-                  </FormItem>
-                )}
-              />
+                <FormField
+                  control={form.control}
+                  name="configuration.EnableGroupingMoviesIntoCollections"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-row items-start space-x-3 space-y-0">
+                      <FormControl>
+                        <Checkbox
+                          checked={field.value}
+                          onCheckedChange={field.onChange}
+                        />
+                      </FormControl>
+                      <div className="space-y-1 leading-none">
+                        <FormLabel>Group movies into collections</FormLabel>
+                        <FormDescription>
+                          Movies in a collection will be displayed as one grouped
+                          item when displaying movie lists.
+                        </FormDescription>
+                      </div>
+                    </FormItem>
+                  )}
+                />
 
-              <FormField
-                control={form.control}
-                name="configuration.EnableGroupingShowsIntoCollections"
-                render={({ field }) => (
-                  <FormItem className="flex flex-row items-start space-x-3 space-y-0">
-                    <FormControl>
-                      <Checkbox
-                        checked={field.value}
-                        onCheckedChange={field.onChange}
-                      />
-                    </FormControl>
-                    <div className="space-y-1 leading-none">
-                      <FormLabel>Group shows into collections</FormLabel>
-                      <FormDescription>
-                        Shows in a collection will be displayed as one grouped
-                        item when displaying show lists.
-                      </FormDescription>
-                    </div>
-                  </FormItem>
-                )}
-              />
+                <FormField
+                  control={form.control}
+                  name="configuration.EnableGroupingShowsIntoCollections"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-row items-start space-x-3 space-y-0">
+                      <FormControl>
+                        <Checkbox
+                          checked={field.value}
+                          onCheckedChange={field.onChange}
+                        />
+                      </FormControl>
+                      <div className="space-y-1 leading-none">
+                        <FormLabel>Group shows into collections</FormLabel>
+                        <FormDescription>
+                          Shows in a collection will be displayed as one grouped
+                          item when displaying show lists.
+                        </FormDescription>
+                      </div>
+                    </FormItem>
+                  )}
+                />
 
-              <FormField
-                control={form.control}
-                name="configuration.EnableExternalContentInSuggestions"
-                render={({ field }) => (
-                  <FormItem className="flex flex-row items-start space-x-3 space-y-0">
-                    <FormControl>
-                      <Checkbox
-                        checked={field.value}
-                        onCheckedChange={field.onChange}
-                      />
-                    </FormControl>
-                    <div className="space-y-1 leading-none">
-                      <FormLabel>
-                        Enable external content in suggestions
-                      </FormLabel>
-                      <FormDescription>
-                        Allow internet trailers and live TV programs to be
-                        included within suggested content.
-                      </FormDescription>
-                    </div>
-                  </FormItem>
-                )}
-              />
+                <FormField
+                  control={form.control}
+                  name="configuration.EnableExternalContentInSuggestions"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-row items-start space-x-3 space-y-0">
+                      <FormControl>
+                        <Checkbox
+                          checked={field.value}
+                          onCheckedChange={field.onChange}
+                        />
+                      </FormControl>
+                      <div className="space-y-1 leading-none">
+                        <FormLabel>
+                          Enable external content in suggestions
+                        </FormLabel>
+                        <FormDescription>
+                          Allow internet trailers and live TV programs to be
+                          included within suggested content.
+                        </FormDescription>
+                      </div>
+                    </FormItem>
+                  )}
+                />
+              </div>
             </div>
           </div>
 
