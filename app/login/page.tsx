db4ae0df753853ec9f3683b92e8a7a488c6f3c66
S.A.M <akhilfilms02@gmail.com>
2026-03-14 11:22:22 -0500

    test fetch instead

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,7 @@
 import { OnboardingFlow } from "@/src/components/onboarding-flow";
-import { GET as getConfig } from "@/app/api/config/route";
 
 export default async function LoginPage() {
-  const res = await getConfig();
+  const res = await fetch("http://localhost:3000/api/config");
   const data = await res.json();
-
   return <OnboardingFlow defaultServerUrl={data.defaultServerUrl} />;
 }
