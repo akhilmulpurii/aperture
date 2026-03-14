e78bcb7a1a0d81828a0165cfe69129af07c5717b
S.A.M <akhilfilms02@gmail.com>
2026-03-14 11:29:18 -0500

    runtime variable

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,6 @@
 import { OnboardingFlow } from "@/src/components/onboarding-flow";
 
 export default async function LoginPage() {
-  const res = await fetch("http://localhost:3000/api/config");
-  const data = await res.json();
-  return <OnboardingFlow defaultServerUrl={data.defaultServerUrl} />;
+  const defaultServerUrl = process.env.DEFAULT_SERVER_URL ?? "";
+  return <OnboardingFlow defaultServerUrl={defaultServerUrl} />;
 }
